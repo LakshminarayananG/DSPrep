b6f3463fd96ab801d576d23bc949d1eb43b2f018
Lakshminarayanan <lakshnarayanan7@gmail.com>
2020-09-30 13:24:55 +0100

    Updated Solution

---
+++ src/asssignments/MedianOfArray.java
@@ -0,0 +1,104 @@
+package asssignments;
+	
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.junit.Test;
+	
+	public class MedianOfArray {
+	
+	// Given two sorted arrays A1, A2. Write a code to find median of the arrays
+	//
+	// Example 1:
+	//
+	// Input:
+	//
+	// A1 = [1,3]
+	// A2 = [2]
+	//
+	// Output: 2
+	//
+	// Example 2:
+	//
+	// Input:
+	//
+	// A1 = [1,2]
+	// A2 = [3,4]
+	//
+	// Output: 2.5
+	
+	
+	//Median-Sum of all numbers/Total Number of numbers
+	//Also equal to n+1/2(middle number) or n+(n+1)/2 (average of middle numbers), if the numbers are sorted.
+	//DataSets
+	@Test
+	public void eg1() {
+		int[] inputOne = { 1, 3 };
+		int[] inputTwo = { 2 };
+		System.out.println("One: " + String.format("%.2f", findMedian1(inputOne, inputTwo)));
+	}
+
+	@Test
+	public void eg2() {
+		int[] inputOne = { 1, 2 };
+		int[] inputTwo = { 3, 4 };
+		System.out.println("Two: " + String.format("%.2f", findMedian1(inputOne, inputTwo)));
+	}
+
+	@Test
+	public void eg3() {
+		int[] inputOne = { 1, 2, 7, 9, 10 };
+		int[] inputTwo = { 3, 4, 11, 56, 88, 90 };
+		System.out.println("Three: " + String.format("%.2f", findMedian1(inputOne, inputTwo)));
+
+	}
+	@Test
+	public void eg4()
+	{
+	int[] inputOne= {-1,2};
+	int[] inputTwo= {3,4,5};
+	System.out.println("Four: "+String.format("%.2f", findMedian1(inputOne,inputTwo)));
+	
+	}
+	@Test
+	public void eg5()
+	{
+	int[] inputOne= {1,1};
+	int[] inputTwo= {3,4};
+	System.out.println("Five: "+String.format("%.2f", findMedian1(inputOne,inputTwo)));
+	}
+	
+	//Solution 1
+	
+	//1. Create a new Array of length equal to the length of array One and Array Two
+	//2. Loop through the first array and add all elements to the new array
+	//3. Loop through the second array and add all elements to the new array
+	//4. sort the array using inbuilt function from collections
+	//5. Determine the length of array list
+	//6. if odd, return n/2
+	//7.if even return (n/2+(n+1)/2)2, where n is the element at the respective index
+	
+	private float findMedian1(int[] firstInput, int[] secondInput) {
+		List<Integer> combinedArray = new ArrayList<>();
+		for (int i = 0; i < firstInput.length; i++)// O[N]
+		{
+			combinedArray.add(firstInput[i]);
+		}
+		for (int i = 0; i < secondInput.length; i++)// O[N]
+		{
+			combinedArray.add(secondInput[i]);
+		}
+		Collections.sort(combinedArray);// O[nlogn]
+
+		int midSize = combinedArray.size() / 2;// O[1]
+
+		if (combinedArray.size() % 2 == 0)// O[1]
+		{
+			return (combinedArray.get(midSize - 1) + combinedArray.get(midSize)) / 2;
+		} else {
+			return combinedArray.get(midSize);
+		}
+
+	}
+	
+	}
---
+++ src/asssignments/StringPermutation.java
@@ -0,0 +1,154 @@
+package asssignments;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+
+public class StringPermutation {
+	
+	//Given two strings s1 and s2, write a function to return true if s2 contains 
+	//the permutation of s1. In other words, one of the first string's permutations 
+	//is the substring of the second string.
+
+	//Input: s1 = "ab" s2 = "eidbaooo"
+	//Output: True
+	//Explanation: s2 contains one permutation of s1 ("ba").
+	//Example 2:
+	//Input:s1= "ab" s2 = "eidboaoo"
+	//Output: False
+
+
+
+		@Test
+		public void TestCase1()
+		{
+			String s1 = "abc";
+			String s2 = "eidbaooo";
+			Boolean output = findPermutation(s1, s2);
+			System.out.println(output);
+		}
+		
+		@Test
+		public void TestCase2()
+		{
+			String s1 = "ab";
+			String s2 = "eidbaooo";
+			Boolean output = findPermutation(s1, s2);
+			System.out.println(output);
+		}
+		
+		@Test
+		public void TestCase3()
+		{
+			String s1 = "abc";
+			String s2 = "abc";
+			Boolean output = findPermutation(s1, s2);
+			System.out.println(output);
+		}
+		
+		@Test
+		public void TestCase4()
+		{
+			String s1 = "abc";
+			String s2 = "e";
+			Boolean output = findPermutation(s1, s2);
+			System.out.println(output);
+		}
+		
+		
+		@Test
+		public void TestCase5()
+		{
+			String s1 = "abx";
+			String s2 = "eidbaooo";
+			Boolean output = findPermutation(s1, s2);
+			System.out.println(output);
+		}
+		
+		@Test
+		public void TestCase6()
+		{
+			String s1 = "a";
+			String s2 = "e";
+			Boolean output = findPermutation(s1, s2);
+			System.out.println(output);
+		}
+		
+		@Test
+		public void TestCase7()
+		{
+			String s1 = "ebanh";
+			String s2 = "eba";
+			Boolean output = findPermutation(s1, s2);
+			System.out.println(output);
+		}
+		
+		@Test
+		public void TestCase8()
+		{
+			String s1 = "abc";
+			String s2 = "ebac";
+			Boolean output = findPermutation(s1, s2);
+			System.out.println(output);
+		}
+		
+	
+	//Brute Force Approach
+		/*
+		 * 1. Declare a characterArray and iterate through String s1 
+		 * 2. Get All possible combinations through another for loop and add all to list 
+		 * 3. Check if length of s2 is less than s1, if so return false 
+		 * 4. Check if both length are equal and if it is, check if list contains s2 and return false 
+		 * 5. Iterate through the string s2 to till s2.length -s1.length 
+		 * 6. Check for substring and see if resulting string is available in the list and return flag
+		 */
+		
+		
+		private Boolean findPermutation(String s1, String s2)
+		{
+			char[] charString = s1.toCharArray();
+			List<String> output = new ArrayList<String>();
+			boolean result=false;
+			
+			for (int i = 0; i < charString.length; i++) 
+			{
+				for (int j = 0; j < charString.length; j++) 
+				{
+					char temp = charString[i];
+					charString[i] = charString[j];
+					charString[j] = temp;
+					if (!output.contains(String.valueOf(charString)))
+					{
+						output.add(String.valueOf(charString));
+					}
+					
+				}
+			}
+			
+			if(s2.length()<s1.length()) {
+				return false;
+			}
+			
+			else if(s2.length()==s1.length()&& output.contains(s2)) {
+				return true;
+			}
+			
+			else {
+			for (int i = 0; i < (s2.length()-s1.length())+1; i++) 
+			{
+				if (output.contains(s2.substring(i, i+s1.length()))) 
+				{
+					result = true;
+					break;
+				}
+				else
+				{
+					result = false;
+				}
+			}
+			}
+			return result;
+		}
+	}
+
+
