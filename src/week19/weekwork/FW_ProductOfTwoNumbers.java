9bd9523c560397d723d3f3ad8337896093d1352c
Lakshminarayanan <lakshnarayanan7@gmail.com>
2021-01-21 14:06:25 +0000

    Latest Changes

--- src/week19/weekwork/FW_ProductOfTwoNumbers.java
+++ src/week19/weekwork/FW_ProductOfTwoNumbers.java
@@ -1,8 +1,45 @@
 package week19.weekwork;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
 public class FW_ProductOfTwoNumbers {
 	
 	
 	/*1) Given list of integers, find 2 numbers which matches the productwithout using map*/
+	
+	@Test
+	public void eg1() {
+		int[] input= {1,4,6,8,2,3,4,5};
+		int target=16;
+		System.out.println(Arrays.toString(retprodIndices(input, target)));
+	}
+	
+	
+	/* Using Set
+	 * 1. Iterate from 0 to n-1
+	 * 2. Check if index value is 0, if so continue to next element (to avoid divide by 0 exception)
+	 * 3. Add index value to set and check if set contains target / index value, if present return both the 
+	 *    values
+	 * 
+	 */
+	
+	private int[] retprodIndices(int[] input, int target) {
+		Set<Integer> set = new HashSet<Integer>();
+		
+		for (int i = 0; i < input.length; i++) {
+			if(input[i]==0) continue;
+			else if(set.contains(target/input[i])) {
+				return new int[] {target/input[i],input[i]};
+			}
+			else {
+				set.add(input[i]);
+			}
+		}
+		throw new RuntimeException("No Matching Numbers");
+	}
 
 }
--- src/week19/weekwork/FW_ValidateBracket.java
+++ src/week19/weekwork/FW_ValidateBracket.java
@@ -1,13 +1,114 @@
 package week19.weekwork;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
+
+import org.junit.Test;
+
 public class FW_ValidateBracket {
 	
 	
-	/*1) Given list of integers, find 2 numbers which matches the productwithout using map*/
+	
 	
 	/*
 	 * 2) seq of paranthesis, validate whether it is valid or not(){}[]<>* char
 	 * count odd then falseisLeftChar(char)isRightChar(char)isPair()
 	 */
+	
+	@Test
+	public void eg1() {
+		String s="{{}}";
+		System.out.println(validatebrackets(s));
+		System.out.println(isvalidParanthesis(s));
+		
+	}
+	
+	@Test
+	public void eg2() {
+		String s="{[>>]}";
+		System.out.println(validatebrackets(s));
+		System.out.println(isvalidParanthesis(s));
+	}
+	
+	@Test
+	public void eg3() {
+		String s="{}[<>]({})";
+		System.out.println(validatebrackets(s));
+		System.out.println(isvalidParanthesis(s));
+	}
+	
+	@Test
+	public void eg4() {
+		String s="{}}";
+		System.out.println(validatebrackets(s));
+		System.out.println(isvalidParanthesis(s));
+	}
+	
+	
+	private boolean validatebrackets(String s) {
+		if(s.length()%2!=0) return false;
+		Map<Character,Character> map = new HashMap<Character,Character>();
+		map.put('(', ')');
+		map.put('{', '}');
+		map.put('[', ']');
+		map.put('<', '>');
+		Stack<Character> stack = new Stack<Character>();
+		for(int i=0;i<s.length();i++) {
+			char ch =s.charAt(i);
+			if(map.containsKey(ch)) {
+				stack.push(map.get(ch));
+			}
+			else if(map.containsValue(ch)) {
+				if(stack.isEmpty()||stack.pop()!=ch) return false;
+			}
+		}
+		return true;
+	}
+	
+	/*
+	 * 1. CHeck the length of the string and if its odd, return false
+	 * 2. Iterate through the string
+	 * 3. If its an opening bracket, add to stack
+	 * 4. If not, check if top element of stack is opening bracket and current char is closing bracket and 
+	 *    the 2 chars put together make a valid string pair of bracket. if so, remove teh top element from stack
+	 * 5. Check if stack is empty & return teh same
+	 */
+	
+	
+	private boolean isvalidParanthesis(String s) {
+		if(s.length()%2!=0) return false;
+		Stack<Character> stack = new Stack<Character>();
+		for(int i=0;i<s.length();i++) {
+			char ch=s.charAt(i);
+			if(stack.isEmpty()||isLeft(ch)) stack.add(ch);
+			else if(isLeft(stack.peek()) && isRight(ch) && isPair(""+stack.peek()+ch)) stack.pop();
+			else return false;
+		}
+		
+		return stack.isEmpty();
+	}
+	
+	
+	
+	
+	private boolean isLeft(char openchar) {
+		Set<Character> set = new HashSet<Character>(Arrays.asList('[','{','<','('));
+		return set.contains(openchar);
+	}
+
+	
+	private boolean isRight(char closechar) {
+		Set<Character> set = new HashSet<Character>(Arrays.asList(']','}','>',')'));
+		return set.contains(closechar);
+	}
+	
+	private boolean isPair(String pair) {
+		Set<String> set = new HashSet<String>(Arrays.asList("[]","{}","()","<>")) ;
+		return set.contains(pair);
+	}
 
 }
